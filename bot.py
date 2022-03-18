0039fbc01a10401f8f1fd745475994e4c312b2ca
dkbrz <glaz.dikobraz@gmail.com>
2022-03-18 12:02:46 +0300

    webhook address

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TOKEN, threaded=False)
 
 
 bot.remove_webhook()
-bot.set_webhook(url="https://stark-garden-80398.herokuapp.com/bot")
+bot.set_webhook(url="https://protected-plateau-34393.herokuapp.com/bot")
 
 app = flask.Flask(__name__)
 
